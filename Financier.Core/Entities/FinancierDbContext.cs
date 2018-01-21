dbe79296016f5c631dd070074d075c139e9655f8
JonnyRivers <jonny.w.rivers@gmail.com>
2018-01-20 17:42:40 -0800

    Added some reminder text to the model builder setup

--- Financier.Core/Entities/FinancierDbContext.cs
+++ Financier.Core/Entities/FinancierDbContext.cs
@@ -11,31 +11,44 @@ namespace Financier.Entities
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // These are required for SQL Server.
+            // Terrifyingly, SQLite doesn't seem to care!
+
+            // WithMany() is required as each Account could appear twice in any AccountRelationship
+            // WithMany() has no lambda because there is no matching navigation property in Account
+            // We use DeleteBehavior.Restrict to prevent attempts to delete an Account that has 
+            // any AccountRelationship references, which would break refential integrity
             modelBuilder.Entity<AccountRelationship>()
                 .HasOne(ar => ar.SourceAccount)
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
-
             modelBuilder.Entity<AccountRelationship>()
                 .HasOne(ar => ar.DestinationAccount)
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
 
+            // WithMany() is required as each Account could appear twice in any BudgetTransaction
+            // WithMany() has no lambda because there is no matching navigation property in Account
+            // We use DeleteBehavior.Restrict to prevent attempts to delete an Account that has 
+            // any BudgetTransaction references, which would break refential integrity
             modelBuilder.Entity<BudgetTransaction>()
                 .HasOne(ar => ar.CreditAccount)
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
-
             modelBuilder.Entity<BudgetTransaction>()
                 .HasOne(ar => ar.DebitAccount)
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
 
+
+            // WithMany() is required as each Account could appear twice in any Transaction
+            // WithMany() has no lambda because there is no matching navigation property in Account
+            // We use DeleteBehavior.Restrict to prevent attempts to delete an Account that has 
+            // any Transaction references, which would break refential integrity
             modelBuilder.Entity<Transaction>()
                 .HasOne(ar => ar.CreditAccount)
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
-
             modelBuilder.Entity<Transaction>()
                 .HasOne(ar => ar.DebitAccount)
                 .WithMany()
