30cf207c6790af451ec4c12a3da1b3deb981261f
JonnyRivers <jonny.w.rivers@gmail.com>
2018-02-03 06:20:06 -0800

    Started on transaction tests

--- Financier.Tests/ViewModelFactoryTests.cs
+++ Financier.Tests/ViewModelFactoryTests.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Financier.Tests
@@ -15,12 +16,12 @@ namespace Financier.Tests
     public class ViewModelFactoryTests
     {
         [TestMethod]
-        public void TestAllViewModelCreation()
+        public void TestAccountViewModelCreation()
         {
             IServiceProvider serviceProvider = BuildServiceProvider();
             IViewModelFactory viewModelFactory = serviceProvider.GetRequiredService<IViewModelFactory>();
 
-            Entities.FinancierDbContext dbContext = 
+            Entities.FinancierDbContext dbContext =
                 serviceProvider.GetRequiredService<Entities.FinancierDbContext>();
             dbContext.Database.EnsureCreated();
 
@@ -48,10 +49,100 @@ namespace Financier.Tests
             IAccountListViewModel accountListViewModel =
                 viewModelFactory.CreateAccountListViewModel();
 
+            Assert.IsNotNull(accountEditViewModel);
             Assert.AreEqual(checkingAccountEntity.AccountId, accountEditViewModel.AccountId);
+
+            Assert.IsNotNull(accountItemViewModel);
             Assert.AreEqual(checkingAccountEntity.AccountId, accountItemViewModel.AccountId);
+
+            Assert.IsNotNull(accountLinkViewModel);
             Assert.AreEqual(checkingAccountEntity.AccountId, accountLinkViewModel.AccountId);
+
+            Assert.IsNotNull(accountListViewModel);
             Assert.AreEqual(dbContext.Accounts.Count(), accountListViewModel.Accounts.Count);
+        }
+
+        [TestMethod]
+        public void TestTransactionViewModelCreation()
+        {
+            IServiceProvider serviceProvider = BuildServiceProvider();
+            IViewModelFactory viewModelFactory = serviceProvider.GetRequiredService<IViewModelFactory>();
+
+            Entities.FinancierDbContext dbContext =
+                serviceProvider.GetRequiredService<Entities.FinancierDbContext>();
+            dbContext.Database.EnsureCreated();
+
+            var currencyFactory = new DbSetup.CurrencyFactory();
+            Entities.Currency usdCurrencyEntity = currencyFactory.Create(DbSetup.CurrencyPrefab.Usd, true);
+            currencyFactory.Add(dbContext, usdCurrencyEntity);
+
+            var accountFactory = new DbSetup.AccountFactory();
+            Entities.Account incomeAccountEntity = accountFactory.Create(DbSetup.AccountPrefab.Income, usdCurrencyEntity);
+            accountFactory.Add(dbContext, incomeAccountEntity);
+            Entities.Account checkingAccountEntity = accountFactory.Create(DbSetup.AccountPrefab.Checking, usdCurrencyEntity);
+            accountFactory.Add(dbContext, checkingAccountEntity);
+
+            dbContext.Transactions.Add(
+                new Entities.Transaction
+                {
+                    CreditAccount = incomeAccountEntity,
+                    DebitAccount = checkingAccountEntity,
+                    Amount = 100,
+                    At = new DateTime(2018,1,1)
+                }
+            );
+            dbContext.SaveChanges();
+
+            IAccountService accountService = serviceProvider.GetRequiredService<IAccountService>();
+            AccountLink incomeAccountLink = accountService.GetAsLink(incomeAccountEntity.AccountId);
+            AccountLink checkingAccountLink = accountService.GetAsLink(checkingAccountEntity.AccountId);
+
+            ITransactionService transactionService = serviceProvider.GetRequiredService<ITransactionService>();
+            Transaction firstTransaction = transactionService.GetAll().First();
+
+            IEnumerable<Transaction> transactions = new Transaction[1]
+            {
+                new Transaction
+                {
+                    CreditAccount = incomeAccountLink,
+                    DebitAccount = checkingAccountLink,
+                    Amount = 30m,
+                    At = new DateTime(2018, 1, 2)
+                }
+            };
+            ITransactionBatchCreateConfirmViewModel transactionBatchCreateConfirmViewModel = 
+                viewModelFactory.CreateTransactionBatchCreateConfirmViewModel(transactions);
+
+            var hint = new Transaction
+            {
+                CreditAccount = incomeAccountLink,
+                DebitAccount = checkingAccountLink,
+                Amount = 50m,
+                At = new DateTime(2018, 1, 3)
+            };
+            ITransactionEditViewModel transactionCreateViewModel = 
+                viewModelFactory.CreateTransactionCreateViewModel(hint);
+
+            int transactionId = firstTransaction.TransactionId;
+            ITransactionEditViewModel transactionEditViewModel = 
+                viewModelFactory.CreateTransactionEditViewModel(transactionId);
+
+            Transaction transaction = firstTransaction;
+            ITransactionItemViewModel transactionItemViewModel = 
+                viewModelFactory.CreateTransactionItemViewModel(transaction);
+
+            ITransactionListViewModel transactionListViewModel = 
+                viewModelFactory.CreateTransactionListViewModel();
+
+            Assert.IsNotNull(transactionBatchCreateConfirmViewModel);
+
+            Assert.IsNotNull(transactionCreateViewModel);
+
+            Assert.IsNotNull(transactionEditViewModel);
+
+            Assert.IsNotNull(transactionItemViewModel);
+
+            Assert.IsNotNull(transactionListViewModel);
         }
 
         private IServiceProvider BuildServiceProvider()
