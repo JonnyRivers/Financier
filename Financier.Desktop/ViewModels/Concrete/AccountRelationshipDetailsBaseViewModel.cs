033dab112a65a4b242d169d7bf5f8a403651776c
JonnyRivers <jonny.w.rivers@gmail.com>
2018-02-25 12:13:21 -0800

    Added base class for account relationship details VM & added tests

---
+++ Financier.Desktop/ViewModels/Concrete/AccountRelationshipDetailsBaseViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Financier.Desktop.Commands;
+using Financier.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Financier.Desktop.ViewModels
+{
+    public abstract class AccountRelationshipDetailsBaseViewModel : IAccountRelationshipDetailsViewModel
+    {
+        protected IAccountService m_accountService;
+        protected IAccountRelationshipService m_accountRelationshipService;
+
+        protected int m_accountRelationshipId;
+
+        protected AccountRelationshipDetailsBaseViewModel(
+            IAccountService accountService,
+            IAccountRelationshipService accountRelationshipService,
+            int accountRelationshipId)
+        {
+            m_accountService = accountService;
+            m_accountRelationshipService = accountRelationshipService;
+
+            m_accountRelationshipId = accountRelationshipId;
+
+            Accounts = accountService.GetAllAsLinks();
+            Types = Enum.GetValues(typeof(AccountRelationshipType)).Cast<AccountRelationshipType>();
+        }
+
+        public IEnumerable<AccountLink> Accounts { get; }
+        public IEnumerable<AccountRelationshipType> Types { get; }
+
+        public AccountLink SourceAccount { get; set; }
+        public AccountLink DestinationAccount { get; set; }
+        public AccountRelationshipType SelectedType { get; set; }
+
+        public AccountRelationship ToAccountRelationship()
+        {
+            return new AccountRelationship
+            {
+                AccountRelationshipId = m_accountRelationshipId,
+                SourceAccount = SourceAccount,
+                DestinationAccount = DestinationAccount,
+                Type = SelectedType
+            };
+        }
+
+        public ICommand OKCommand => new RelayCommand(OKExecute);
+        public ICommand CancelCommand => new RelayCommand(CancelExecute);
+
+        protected abstract void OKExecute(object obj);
+
+        private void CancelExecute(object obj)
+        {
+
+        }
+    }
+}
--- Financier.Desktop/ViewModels/Concrete/AccountRelationshipEditViewModel.cs
+++ Financier.Desktop/ViewModels/Concrete/AccountRelationshipEditViewModel.cs
@@ -1,35 +1,20 @@
-﻿using Financier.Desktop.Commands;
-using Financier.Services;
+﻿using Financier.Services;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Input;
 
 namespace Financier.Desktop.ViewModels
 {
-    public class AccountRelationshipEditViewModel : IAccountRelationshipDetailsViewModel
+    public class AccountRelationshipEditViewModel : AccountRelationshipDetailsBaseViewModel
     {
         private ILogger<AccountRelationshipEditViewModel> m_logger;
-        private IAccountService m_accountService;
-        private IAccountRelationshipService m_accountRelationshipService;
-
-        private int m_accountRelationshipId;
 
         public AccountRelationshipEditViewModel(
             ILogger<AccountRelationshipEditViewModel> logger,
             IAccountService accountService,
             IAccountRelationshipService accountRelationshipService,
-            int accountRelationshipId)
+            int accountRelationshipId) : base(accountService, accountRelationshipService, accountRelationshipId)
         {
             m_logger = logger;
-            m_accountService = accountService;
-            m_accountRelationshipService = accountRelationshipService;
-
-            Accounts = accountService.GetAllAsLinks();
-            Types = Enum.GetValues(typeof(AccountRelationshipType)).Cast<AccountRelationshipType>();
-
-            m_accountRelationshipId = accountRelationshipId;
 
             AccountRelationship accountRelationship = m_accountRelationshipService.Get(accountRelationshipId);
 
@@ -38,37 +23,11 @@ namespace Financier.Desktop.ViewModels
             SelectedType = accountRelationship.Type;
         }
 
-        public IEnumerable<AccountLink> Accounts { get; }
-        public IEnumerable<AccountRelationshipType> Types { get; }
-
-        public AccountLink SourceAccount { get; set; }
-        public AccountLink DestinationAccount { get; set; }
-        public AccountRelationshipType SelectedType { get; set; }
-
-        public ICommand OKCommand => new RelayCommand(OKExecute);
-        public ICommand CancelCommand => new RelayCommand(CancelExecute);
-
-        private void OKExecute(object obj)
+        protected override void OKExecute(object obj)
         {
             AccountRelationship accountRelationship = ToAccountRelationship();
 
             m_accountRelationshipService.Update(accountRelationship);
-        }
-
-        private void CancelExecute(object obj)
-        {
-
-        }
-
-        public AccountRelationship ToAccountRelationship()
-        {
-            return new AccountRelationship
-            {
-                AccountRelationshipId = m_accountRelationshipId,
-                SourceAccount = SourceAccount,
-                DestinationAccount = DestinationAccount,
-                Type = SelectedType
-            };
         }
     }
 }
