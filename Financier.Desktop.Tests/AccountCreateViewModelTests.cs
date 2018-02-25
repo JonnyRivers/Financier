eb1d796ec0e202d60ceab8ff7bf378b1a57f0b51
JonnyRivers <jonny.w.rivers@gmail.com>
2018-02-25 11:44:14 -0800

    Added base class for account details VM & added tests

---
+++ Financier.Desktop.Tests/AccountCreateViewModelTests.cs
@@ -0,0 +1,91 @@
+﻿using Financier.Desktop.ViewModels;
+using Financier.Services;
+using Financier.UnitTesting;
+using Financier.UnitTesting.DbSetup;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Financier.Desktop.Tests
+{
+    [TestClass]
+    public class AccountCreateViewModelTests
+    {
+        [TestMethod]
+        public void TestAccountCreateViewModelCancel()
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+
+            using (var sqliteMemoryWrapper = new SqliteMemoryWrapper())
+            {
+                var currencyFactory = new CurrencyFactory();
+                var usdCurrencyEntity = currencyFactory.Create(CurrencyPrefab.Usd, true);
+                currencyFactory.Add(sqliteMemoryWrapper.DbContext, usdCurrencyEntity);
+
+                var accountService = new AccountService(
+                    loggerFactory.CreateLogger<AccountService>(),
+                    sqliteMemoryWrapper.DbContext);
+
+                var currencyService = new CurrencyService(
+                    loggerFactory.CreateLogger<CurrencyService>(),
+                    sqliteMemoryWrapper.DbContext);
+
+                var viewModel = new AccountCreateViewModel(
+                    loggerFactory.CreateLogger<AccountCreateViewModel>(),
+                    accountService,
+                    currencyService
+                );
+
+                viewModel.Name = "MyAccount";
+                viewModel.SelectedAccountType = AccountType.Asset;
+                viewModel.SelectedAccountSubType = AccountSubType.Investment;
+                viewModel.CancelCommand.Execute(this);
+
+                List<Account> accounts = accountService.GetAll().ToList();
+
+                Assert.AreEqual(0, accounts.Count);
+            }
+        }
+
+        [TestMethod]
+        public void TestAccountCreateViewModelOK()
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+
+            using (var sqliteMemoryWrapper = new SqliteMemoryWrapper())
+            {
+                var currencyFactory = new CurrencyFactory();
+                var usdCurrencyEntity = currencyFactory.Create(CurrencyPrefab.Usd, true);
+                currencyFactory.Add(sqliteMemoryWrapper.DbContext, usdCurrencyEntity);
+
+                var accountService = new AccountService(
+                    loggerFactory.CreateLogger<AccountService>(),
+                    sqliteMemoryWrapper.DbContext);
+
+                var currencyService = new CurrencyService(
+                    loggerFactory.CreateLogger<CurrencyService>(),
+                    sqliteMemoryWrapper.DbContext);
+
+                var viewModel = new AccountCreateViewModel(
+                    loggerFactory.CreateLogger<AccountCreateViewModel>(),
+                    accountService,
+                    currencyService
+                );
+
+                viewModel.Name = "MyAccount";
+                viewModel.SelectedAccountType = AccountType.Asset;
+                viewModel.SelectedAccountSubType = AccountSubType.Investment;
+                viewModel.OKCommand.Execute(this);
+
+                List<Account> accounts = accountService.GetAll().ToList();
+
+                Assert.AreEqual(1, accounts.Count);
+                Assert.AreEqual(usdCurrencyEntity.CurrencyId, accounts[0].Currency.CurrencyId);
+                Assert.AreEqual(viewModel.Name, accounts[0].Name);
+                Assert.AreEqual(viewModel.SelectedAccountType, accounts[0].Type);
+                Assert.AreEqual(viewModel.SelectedAccountSubType, accounts[0].SubType);
+            }
+        }
+    }
+}
--- Financier.Desktop/ViewModels/Concrete/AccountEditViewModel.cs
+++ Financier.Desktop/ViewModels/Concrete/AccountEditViewModel.cs
@@ -1,36 +1,22 @@
-﻿using Financier.Desktop.Commands;
-using Financier.Services;
+﻿using Financier.Services;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Input;
 
 namespace Financier.Desktop.ViewModels
 {
-    public class AccountEditViewModel : IAccountDetailsViewModel
+    public class AccountEditViewModel : AccountDetailsBaseViewModel
     {
         private ILogger<AccountEditViewModel> m_logger;
-        private IAccountService m_accountService;
-        private ICurrencyService m_currencyService;
-        private int m_accountId;
 
         public AccountEditViewModel(
             ILogger<AccountEditViewModel> logger,
             IAccountService accountService, 
             ICurrencyService currencyService,
-            int accountId)
+            int accountId) : base(accountService, currencyService, accountId)
         {
             m_logger = logger;
-            m_accountService = accountService;
-            m_currencyService = currencyService;
 
-            AccountTypes = Enum.GetValues(typeof(AccountType)).Cast<AccountType>();
-            AccountSubTypes = Enum.GetValues(typeof(AccountSubType)).Cast<AccountSubType>();
-            Currencies = m_currencyService.GetAll();
-
-            m_accountId = accountId;
-            Account account = m_accountService.Get(accountId);
+            Account account = m_accountService.Get(m_accountId);
 
             Name = account.Name;
             SelectedAccountType = account.Type;
@@ -38,45 +24,11 @@ namespace Financier.Desktop.ViewModels
             SelectedCurrency = Currencies.Single(c => c.CurrencyId == account.Currency.CurrencyId);
         }
 
-        public Account ToAccount()
-        {
-            return new Account
-            {
-                AccountId = m_accountId,
-                Name = Name,
-                Type = SelectedAccountType,
-                SubType = SelectedAccountSubType,
-                Currency = SelectedCurrency
-            };
-        }
-
-        public IEnumerable<AccountType> AccountTypes { get; }
-        public IEnumerable<AccountSubType> AccountSubTypes { get; }
-        public IEnumerable<Currency> Currencies { get; }
-
-        public int AccountId
-        {
-            get { return m_accountId; }
-        }
-
-        public string Name { get; set; }
-        public AccountType SelectedAccountType { get; set; }
-        public AccountSubType SelectedAccountSubType { get; set; }
-        public Currency SelectedCurrency { get; set; }
-
-        public ICommand OKCommand => new RelayCommand(OKExecute);
-        public ICommand CancelCommand => new RelayCommand(CancelExecute);
-
-        private void OKExecute(object obj)
+        protected override void OKExecute(object obj)
         {
             Account account = ToAccount();
 
             m_accountService.Update(account);
-        }
-
-        private void CancelExecute(object obj)
-        {
-
         }
     }
 }
