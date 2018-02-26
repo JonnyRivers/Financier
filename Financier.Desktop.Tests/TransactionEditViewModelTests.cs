2306770271b0c675068a29d133008ba2d19ab111
JonnyRivers <jonny.w.rivers@gmail.com>
2018-02-25 16:46:29 -0800

    Added failing unit tests for transaction detail view models

---
+++ Financier.Desktop.Tests/TransactionEditViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using Financier.Desktop.ViewModels;
+using Financier.Services;
+using Financier.UnitTesting;
+using Financier.UnitTesting.DbSetup;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Financier.Desktop.Tests
+{
+    [TestClass]
+    public class TransactionEditViewModelTests
+    {
+        [TestMethod]
+        public void TestTransactionEditViewModelCancel()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void TestTransactionEditViewModelOK()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
