53358a6aae4a1e0961bfbe0a48b1bedaa02a6413
JonnyRivers <jonny.w.rivers@gmail.com>
2018-11-20 12:56:44 -0800

    Added beginnings of json serialization

--- Financier.Core/Services/Concrete/LocalDatabaseConnectionService.cs
+++ Financier.Core/Services/Concrete/LocalDatabaseConnectionService.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Financier.Services
 {
     public class LocalDatabaseConnectionService : IDatabaseConnectionService
     {
-        private List<DatabaseConnection> m_databaseConnetions;
+        private string m_path;
+        private List<DatabaseConnection> m_databaseConnections;
 
         public LocalDatabaseConnectionService()
         {
-            m_databaseConnetions = new List<DatabaseConnection>();
+            string localApplicationDataDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string financierDataDirectory = Path.Combine(localApplicationDataDirectory, "Financier");
+            if (!Directory.Exists(financierDataDirectory))
+                Directory.CreateDirectory(financierDataDirectory);
+            m_path = Path.Combine(financierDataDirectory, "DatabaseConnections.json");
+
+            Load();
+            Save();
         }
 
         public void Create(DatabaseConnection databaseConnection)
@@ -24,17 +36,38 @@ namespace Financier.Services
 
         public DatabaseConnection Get(int databaseConnectionId)
         {
-            throw new NotImplementedException();
+            return m_databaseConnections.SingleOrDefault(dbc => dbc.DatabaseConnectionId == databaseConnectionId);
         }
 
         public IEnumerable<DatabaseConnection> GetAll()
         {
-            return m_databaseConnetions;
+            return m_databaseConnections;
         }
 
         public void Update(DatabaseConnection databaseConnection)
         {
             throw new NotImplementedException();
+        }
+
+        private void Load()
+        {
+            if(File.Exists(m_path))
+            {
+                string fileContents = File.ReadAllText(m_path);
+                JArray jsonObject = JArray.Parse(fileContents);
+                m_databaseConnections = jsonObject.ToObject<List<DatabaseConnection>>();
+            }
+            else
+            {
+                m_databaseConnections = new List<DatabaseConnection>();
+            }
+        }
+
+        private void Save()
+        {
+            JArray jsonObject = (JArray)JToken.FromObject(m_databaseConnections);
+            string fileContents = jsonObject.ToString();
+            File.WriteAllText(m_path, fileContents);
         }
     }
 }
--- Financier.Core/Services/Model/DatabaseConnection.cs
+++ Financier.Core/Services/Model/DatabaseConnection.cs
@@ -8,8 +8,22 @@ namespace Financier.Services
         public string Name { get; set; }
         public DatabaseConnectionType Type { get; set; }
 
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string UserId { get; set; }
+
         public string BuildConnectionString()
         {
+            if(Type == DatabaseConnectionType.SqlServer)
+            {
+                return $"Server={Server};Database={Database};Persist Security Info=False;User ID={UserId};" + 
+                    "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            }
+            else if (Type == DatabaseConnectionType.SqlLiteFile)
+            {
+                return $"Server=(localdb)\\mssqllocaldb;Database={Database};Trusted_Connection=True;";
+            }
+
             throw new NotImplementedException();
         }
     }
