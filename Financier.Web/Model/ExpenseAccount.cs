aa7c45bbd86324aeea793f731bf0ce8e59dfff21
JonnyRivers <jonny.w.rivers@gmail.com>
2020-02-27 22:07:16 -0800

    Added back-end for expenses onteh Web API (#115)

---
+++ Financier.Web/Model/ExpenseAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Financier.Web.Model
+{
+    public class ExpenseAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public IEnumerable<ExpenseTransaction> RecentTransactions { get; set; }
+    }
+}
