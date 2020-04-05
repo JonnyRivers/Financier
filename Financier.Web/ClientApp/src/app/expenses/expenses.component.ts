d177f56c3f2eb6740b3bdc1f2cd68604aa4c8c78
JonnyRivers <jonny.w.rivers@gmail.com>
2020-04-05 10:18:14 -0700

    MVP for expenses page

--- Financier.Web/ClientApp/src/app/expenses/expenses.component.ts
+++ Financier.Web/ClientApp/src/app/expenses/expenses.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
   styleUrls: ['./expenses.component.css']
 })
-export class ExpensesComponent implements OnInit {
+export class ExpensesComponent {
+    public expenseAccounts: ExpenseAccount[];
 
-  constructor() { }
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        http.get<ExpenseAccount[]>(baseUrl + 'api/expenses').subscribe(result => {
+            this.expenseAccounts = result.sort((a, b) => (a.name > b.name ? 1 : -1));
+        }, error => console.error(error));
+    }
+}
 
-  ngOnInit() {
-  }
+interface ExpenseAccount {
+    id: number;
+    name: string;
+    balance: number;
+    recentTransactions: ExpenseTransaction[];
+}
 
+interface ExpenseTransaction {
+    id: number;
+    at: Date;
+    otherAccountName: string;
+    amount: number;
+    balance: number;
 }
