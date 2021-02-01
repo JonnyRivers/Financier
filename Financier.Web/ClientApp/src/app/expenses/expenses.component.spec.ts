98ce4c8d94b7725c7de8381bc7773d2bd0f98d3b
JonnyRivers <jonny.w.rivers@gmail.com>
2021-01-31 18:38:08 -0800

    Upgrade angular from 10.1.2 to 11.1.1 (#135)

--- Financier.Web/ClientApp/src/app/expenses/expenses.component.spec.ts
+++ Financier.Web/ClientApp/src/app/expenses/expenses.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ExpensesComponent } from './expenses.component';
 
@@ -6,7 +6,7 @@ describe('ExpensesComponent', () => {
   let component: ExpensesComponent;
   let fixture: ComponentFixture<ExpensesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ExpensesComponent ]
     })
