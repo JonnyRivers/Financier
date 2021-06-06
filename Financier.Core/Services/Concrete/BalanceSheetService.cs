a6161b04acce25355c9b38d913f1b3e4b0a74d57
JonnyRivers <jonny.w.rivers@gmail.com>
2021-06-06 12:36:41 -0700

    Stale accounts are now pruned from Balance Sheet (#141)

--- Financier.Core/Services/Concrete/BalanceSheetService.cs
+++ Financier.Core/Services/Concrete/BalanceSheetService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,46 +8,98 @@ namespace Financier.Services
 {
     public class BalanceSheetService : IBalanceSheetService
     {
-        ILogger<BalanceSheetService> m_logger;
-        IAccountService m_accountService;
-        ICurrencyService m_currencyService;
+        private ILogger<BalanceSheetService> m_logger;
+        private Entities.FinancierDbContext m_dbContext;
 
         public BalanceSheetService(
             ILoggerFactory loggerFactory,
-            IAccountService accountService, 
-            ICurrencyService currencyService)
+            Entities.FinancierDbContext dbContext)
         {
             m_logger = loggerFactory.CreateLogger<BalanceSheetService>();
-            m_accountService = accountService;
-            m_currencyService = currencyService;
+            m_dbContext = dbContext;
         }
 
         public BalanceSheet Generate(DateTime at)
         {
-            Currency primaryCurrency = m_currencyService.GetPrimary();
+            string primaryCurrencySymbol = m_dbContext.Currencies.Single(c => c.IsPrimary).Symbol;
 
-            List<Account> accounts = m_accountService.GetAllPhysical().ToList();
+            var logicalAccountIds = new HashSet<int>(
+                m_dbContext.AccountRelationships
+                    .Where(ar => ar.Type == AccountRelationshipType.PhysicalToLogical)
+                    .Select(ar => ar.DestinationAccountId)
+            );
+
+            List<Entities.Account> allPhysicalAccountEntities = m_dbContext.Accounts
+                .Include(a => a.Currency)
+                .Where(a => a.Type == AccountType.Asset || a.Type == AccountType.Liability)
+                .Where(a => !logicalAccountIds.ToList().Contains(a.AccountId))
+                .ToList();
+
+            List<int> allPhysicalAccountIds = allPhysicalAccountEntities.Select(a => a.AccountId).ToList();
+
+            List<Entities.AccountRelationship> allRelatedPhysicalLogicalRelationships = m_dbContext.AccountRelationships
+                    .Where(r => allPhysicalAccountIds.Contains(r.SourceAccountId) &&
+                                r.Type == AccountRelationshipType.PhysicalToLogical)
+                    .ToList();
+            List<int> allRelatedLogicalAccountIds = allRelatedPhysicalLogicalRelationships.Select(r => r.DestinationAccountId).ToList();
+
+            List<int> allAccountIds = new HashSet<int>(allPhysicalAccountIds.Concat(allRelatedLogicalAccountIds)).ToList();
+
+            List<Entities.Transaction> allRelatedCreditTransactions =
+                m_dbContext.Transactions
+                    .Where(t => allAccountIds.Contains(t.CreditAccountId) &&
+                                t.At <= at)
+                    .ToList();
+            List<Entities.Transaction> allRelatedDebitTransactions =
+                m_dbContext.Transactions
+                    .Where(t => allAccountIds.Contains(t.DebitAccountId) &&
+                                t.At <= at)
+                    .ToList();
 
             var assets = new List<BalanceSheetItem>();
             var liabilities = new List<BalanceSheetItem>();
-            foreach (Account account in accounts)
+            foreach (Entities.Account physicalAccountEntity in allPhysicalAccountEntities)
             {
+                IEnumerable<int> relatedLogicalAccountIds = allRelatedPhysicalLogicalRelationships
+                    .Where(r => r.SourceAccountId == physicalAccountEntity.AccountId)
+                    .Select(r => r.DestinationAccountId);
+
+                var accountIds = new List<int>();
+                accountIds.Add(physicalAccountEntity.AccountId);
+                accountIds.AddRange(relatedLogicalAccountIds);
+
+                IEnumerable<Entities.Transaction> creditTransactions = allRelatedCreditTransactions
+                    .Where(t => accountIds.Contains(t.CreditAccountId));
+                IEnumerable<Entities.Transaction> debitTransactions = allRelatedDebitTransactions
+                    .Where(t => accountIds.Contains(t.DebitAccountId));
+
+                // get most recent transaction
+                IEnumerable<Entities.Transaction> allTransactions = creditTransactions.Concat(debitTransactions).OrderByDescending(t => t.At);
+                if (!allTransactions.Any())
+                    continue;
+
+                // calculate balance
+                decimal creditBalance = creditTransactions.Sum(t => t.Amount);
+                decimal debitBalance = debitTransactions.Sum(t => t.Amount);
+                decimal balance = debitBalance - creditBalance;
+
                 var item = new BalanceSheetItem(
-                    account.Name, 
-                    m_accountService.GetBalanceAt(account.AccountId, at, true)
+                    physicalAccountEntity.Name,
+                    balance,
+                    allTransactions.Select(t => t.At).First()
                 );
 
-                if(account.Type == AccountType.Asset)
+                if(physicalAccountEntity.Type == AccountType.Asset)
                 {
                     assets.Add(item);
                 }
-                else if (account.Type == AccountType.Liability)
+                else if (physicalAccountEntity.Type == AccountType.Liability)
                 {
                     liabilities.Add(item);
                 }
             }
 
-            return new BalanceSheet(primaryCurrency.Symbol, assets, liabilities);
+            return new BalanceSheet(primaryCurrencySymbol, assets, liabilities);
         }
     }
 }
